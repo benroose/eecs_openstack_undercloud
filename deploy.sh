8f182050e27eb9cd8da6e12a2ca3e63941b06619
Ben Roose <ben.roose@wichita.edu>
2017-07-06 21:56:34 -0400

    attempt to deploy with full script

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ time openstack overcloud deploy --templates \
         -e /home/stack/templates/enable-tls.yaml \
         -e /home/stack/templates/cloudname.yaml \
         -e /home/stack/templates/inject-trust-anchor.yaml \
-        -e /home/stack/templates/ceilometer.yaml \
         -e /usr/share/openstack-tripleo-heat-templates/environments/tls-endpoints-public-dns.yaml \
+        -e /home/stack/templates/ceilometer.yaml \
         -e /home/stack/templates/fernet.yaml \
 	-e /home/stack/templates/puppet-ceph-external.yaml \
 	-e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \

29ac8e6ddf89d3ab3a3d218f21e869e23871265f
Ben Roose <ben.roose@wichita.edu>
2017-07-07 20:24:42 -0400

    removing TLS templates from deploy.sh and commenting out NetworkDeploymentActions

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ cd ~
 source ~/stackrc
 
 #       -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-radosgw.yaml \
+#        -e /home/stack/templates/enable-tls.yaml \
+#        -e /home/stack/templates/inject-trust-anchor.yaml \
+#        -e /usr/share/openstack-tripleo-heat-templates/environments/tls-endpoints-public-dns.yaml \
 
 time openstack overcloud deploy --templates \
         -e /home/stack/templates/timezone.yaml \
-        -e /home/stack/templates/enable-tls.yaml \
         -e /home/stack/templates/cloudname.yaml \
-        -e /home/stack/templates/inject-trust-anchor.yaml \
-        -e /usr/share/openstack-tripleo-heat-templates/environments/tls-endpoints-public-dns.yaml \
         -e /home/stack/templates/ceilometer.yaml \
         -e /home/stack/templates/fernet.yaml \
 	-e /home/stack/templates/puppet-ceph-external.yaml \
